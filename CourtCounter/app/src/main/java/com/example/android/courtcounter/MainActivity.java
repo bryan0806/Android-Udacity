8910b93eb33b91cb7befb3571e275e39072329db
bryan.liu@eys3d.com <bryan.liu@eys3d.com>
2016-10-25 11:18:13 +0800

    Adding functions to team b like before

--- CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -8,11 +8,13 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     int scoreTeamA = 0;
+    int scoreTeamB = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
     }
 
     /**
@@ -23,9 +25,22 @@ public class MainActivity extends AppCompatActivity {
         scoreView.setText(String.valueOf(score));
     }
 
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
     public void threepoint(View view){
         scoreTeamA = scoreTeamA+3;
         displayForTeamA(scoreTeamA);
+    }
+
+    public void teamB_threepoint(View view){
+        scoreTeamB = scoreTeamB+3;
+        displayForTeamB(scoreTeamB);
     }
 
     public void twopoint(View view)
@@ -34,8 +49,22 @@ public class MainActivity extends AppCompatActivity {
         displayForTeamA(scoreTeamA);
     }
 
+    public void teamB_twopoint(View view)
+    {
+        scoreTeamB = scoreTeamB+2;
+        displayForTeamB(scoreTeamB);
+    }
+
     public void freeThrow(View view){
         scoreTeamA = scoreTeamA+1;
         displayForTeamA(scoreTeamA);
     }
+
+    public void teamB_freeThrow(View view){
+        scoreTeamB = scoreTeamB+1;
+        displayForTeamB(scoreTeamB);
+    }
+
+
+
 }
