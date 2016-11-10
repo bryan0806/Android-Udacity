f59916e7e174f377559c2ce125f073adce633592
bryan.liu@eys3d.com <bryan.liu@eys3d.com>
2016-11-10 18:46:07 +0800

    Using only Java code to change the word display...

--- CallingMethods/app/src/main/java/com/example/android/callingmethods/MainActivity.java
+++ CallingMethods/app/src/main/java/com/example/android/callingmethods/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.android.callingmethods;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        TextView text = new TextView(this);
+        text.setText("wow!");
+        text.setTextColor(Color.BLUE);
+        text.setTextSize(55);
+        setContentView(text);
     }
 }
