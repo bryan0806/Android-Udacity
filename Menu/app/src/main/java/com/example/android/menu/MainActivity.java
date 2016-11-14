5e087dddb479ffd73f4f002470f055069256e4c8
bryan.liu@eys3d.com <bryan.liu@eys3d.com>
2016-11-14 17:25:58 +0800

    Log the first textview into catlog

--- Menu/app/src/main/java/com/example/android/menu/MainActivity.java
+++ Menu/app/src/main/java/com/example/android/menu/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.android.menu;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -15,7 +17,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void printToLogs(View view) {
         // Find first menu item TextView and print the text to the logs
-        
+        TextView textViewItem1 = (TextView) findViewById(R.id.menu_item_1);
+        String menuItem1 = textViewItem1.getText().toString();
+        Log.v("MainActivity", menuItem1);
         // Find second menu item TextView and print the text to the logs
 
         // Find third menu item TextView and print the text to the logs
